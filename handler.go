6ec3c523859855e9960c72518ea6fcd934d4542b
Kyoichiro Yamada <yamada@wacul.co.jp>
2017-06-13 07:42:23 +0900

    Add Collector

--- handler.go
+++ handler.go
@@ -132,6 +132,21 @@ type doerImpl func() error
 
 func (d doerImpl) Do() error { return d() }
 
+type Collector []Doer
+
+func (c Collector) Do() error {
+	for _, d := range c {
+		if err := d.Do(); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (c *Collector) Push(d Doer) {
+	*c = append(*c, d)
+}
+
 type Parser func(string) error
 
 func (h *Handler) Ask(parse Parser) error {
